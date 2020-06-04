0f26d0b5fce0990171a64742425552152adc4f03
ramown <emmanuelrlima@gmail.com>
2020-06-04 00:10:27 -0300

    example using Flask-Login

---
+++ app/auth.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+from flask import flash
+from flask_login import login_user, logout_user, login_required
+from . import db
+from .models import User
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            login_user(user, remember=remember)
+            return redirect(url_for('main.profile'))
+        flash('Please check your login details and try again.')
+    return render_template('login.html')
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email address already exists')
+            return redirect(url_for('auth.signup'))
+
+        new_user = User(email=email, name=name, password=password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+    return render_template('signup.html')
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
---
+++ run.py
@@ -0,0 +1,7 @@
+from app import create_app, db
+
+app = create_app()
+db.create_all(app=create_app())
+
+if __name__ == '__main__':
+	app.run()
